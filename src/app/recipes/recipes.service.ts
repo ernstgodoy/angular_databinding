228e51d865c4e714941e388c84265df97dbba4bc
ernesto <ernsto.godoy@gmail.com>
2020-10-03 14:41:03 -0700

    services

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,15 +1,39 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model'
 
+@Injectable()
 export class RecipesService {
   recipeSelected = new EventEmitter<Recipe>()
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'Schnitzel', 
+      'This is simply a test', 
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('meat', 1),
+        new Ingredient('fries', 20)
+      ]),
+    new Recipe(
+      'Burger', 
+      'This is simply a test', 
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('meat', 1),
+        new Ingredient('buns', 2)
+      ])
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice()
+  }
+
+  addIngretientsToList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients)
+
   }
 }
